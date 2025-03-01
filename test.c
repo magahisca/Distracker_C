dd8861ace9092dcb99a8bb1c631790024c29e55c
Carlos Magahis <carlosandrei.magahis@gmail.com>
2025-03-02 00:20:28 +0800

    added initial time check

--- test.c
+++ test.c
@@ -8,6 +8,26 @@ int main()
 {
 char WindowTitle[256];
 char LastWindowTitle[256];
+time_t now = time(NULL); //initialize time
+
+//get time
+
+//get local time
+struct tm *curr_Time = localtime(&now);
+int sec = curr_Time->tm_sec;
+int min = curr_Time->tm_min;
+int hour = curr_Time->tm_hour;
+int mday = curr_Time->tm_mday;
+int mon = curr_Time->tm_mon;
+int year = curr_Time->tm_year;
+printf("%d", sec);
+printf("%d", min);
+printf("%d", hour);
+printf("%d", mday);
+printf("%d", mon);
+printf("%d", year);
+printf("\n");
+
 
 while (1){
     HWND hwnd = GetForegroundWindow();
@@ -19,6 +39,7 @@ while (1){
             strcpy(LastWindowTitle, WindowTitle);
 
         }
+
 
 
     }
