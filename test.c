540f16fabc2cac68ae5b83802b4679dba613d962
Carlos Magahis <carlosandrei.magahis@gmail.com>
2025-03-07 17:01:39 +0800

    Revised start and end time variables and calls, added duration variable

--- test.c
+++ test.c
@@ -2,50 +2,63 @@
 #include <windows.h>
 #include <string.h>
 #include <time.h>
+//initialize time
+void getStartTime(time_t *startTime){
+     *startTime = time(NULL);
+}
+void getEndTime(time_t *endTime){
+    *endTime = time(NULL);
+}
 
 
 int main()
 {
 char WindowTitle[256];
-char LastWindowTitle[256];
-time_t now = time(NULL); //initialize time
-
-//get time
-
+char LastWindowTitle[256] = "";
+time_t startTime, endTime;
+getStartTime(&startTime);
+//get readable time
+char *readableTime = ctime(&startTime);
+printf("%s", readableTime);
 //get local time
-struct tm *curr_Time = localtime(&now);
+
+
+struct tm *curr_Time = localtime(&startTime);
+struct tm *startLocalTime = localtime(&startTime); 
 int sec = curr_Time->tm_sec;
 int min = curr_Time->tm_min;
 int hour = curr_Time->tm_hour;
 int mday = curr_Time->tm_mday;
-int mon = curr_Time->tm_mon;
-int year = curr_Time->tm_year;
-printf("%d", sec);
-printf("%d", min);
-printf("%d", hour);
-printf("%d", mday);
-printf("%d", mon);
-printf("%d", year);
-printf("\n");
-
+int mon = curr_Time->tm_mon +1;
+int year = curr_Time->tm_year + 1900;
+printf("Program start: %02d/%02d/%d %02d:%02d:%02d\n", mon, mday, year, hour, min, sec);
 
 while (1){
     HWND hwnd = GetForegroundWindow();
     if (hwnd){
         GetWindowText(hwnd, WindowTitle, sizeof(WindowTitle));
-
+        
         if (strcmp(WindowTitle, LastWindowTitle) != 0){
+            getEndTime(&endTime);
+            char *endTimeReadable = ctime(&endTime);
+            struct tm *endLocalTime = localtime(&endTime);
+            if (strlen(LastWindowTitle)>0){
+                double duration = difftime(endTime, startTime);
+                printf("You spent %.2f seconds on %s \n", duration, LastWindowTitle);
+            }
+            printf("End time for %s: %s",LastWindowTitle, endTimeReadable);
+
             printf("Active Window: %s\n", WindowTitle);
+            
             strcpy(LastWindowTitle, WindowTitle);
-
+            getStartTime(&startTime);    
+            char *startTimeReadable = ctime(&startTime);
+            struct tm *startLocalTime = localtime(&startTime);
+            printf("%s", startTimeReadable);
         }
-
-
-
     }
 
-
-
+Sleep(500); 
 }
 
 
